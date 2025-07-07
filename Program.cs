3ccf4ffa5f1a4c180e887dac5ef03aef58e41403
Hemanth kumar lingabathina <buntytwo@proton.me>
2025-07-08 00:31:50 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
-﻿using System;
-namespace ubuntu {
-	class Public {
-		public static void Main(string[] args){
-			Console.WriteLine("this is hemanth");
-		}
-	}
-}
+﻿int number  = 100;
+Console.WriteLine(number);
+string name  = "this is hemanth kumar lingabathina";
+Console.WriteLine(name);
+double pi =	 0.90;
+Console.WriteLine(pi);
+int num=100;
+num = 500;
+Console.Write(num);
+bool mybool = true;
+// bool lbool = false;
+Console.WriteLine(mybool);
+const int numbers = 100;
+Console.WriteLine(numbers);
+string names = "hemanthkumar";
+Console.WriteLine(names+ "lingabathina");
+string firstname = "hemanthkumar";
+string lastname = "lingabathina";
+string fullname = firstname + lastname;
+Console.WriteLine(fullname);
+string fullass = "hemanthkumarlingabathina";
+Console.WriteLine("fullass");
+
+
+
+
+
+
+
+
+
+
 
 
 
