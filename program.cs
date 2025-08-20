eaf4bb0d5c083736a376af1115212da5098fb46b
Hemanth kumar lingabathina <buntytwo@proton.me>
2025-08-20 14:07:10 +0530

    basic c sharp code

---
+++ program.cs
@@ -0,0 +1,38 @@
+using System;
+namespace bunty {
+
+	class Public {
+
+	static void Main(string[] args) {
+		Console.WriteLine("enter your name sir:" );
+		string name  = Console.ReadLine();
+		Console.WriteLine("the name is: " + name);
+
+
+
+		Console.WriteLine("enter the other name sir:" );
+		string name2  = Console.ReadLine();
+		Console.WriteLine("the other name is" + name2);
+
+
+         	Console.WriteLine("The names are " + name +  name2);
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+	}
+
+
+
+
+
+}
